4cc9ab2c6e781c1c2eee6d416ccc309f3e03b886
Oleh Solonynka <soloninko.oleg@gmail.com>
2022-10-21 02:42:56 +0300

    clean up

--- SeriousSoftware.TestTask/Program.cs
+++ SeriousSoftware.TestTask/Program.cs
@@ -13,11 +13,6 @@ builder.Services.AddScoped<IPerformanceCalculator, PerformanceCalculator>();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.Configure<PolygonApiSettings>(builder.Configuration.GetSection("PolygonApi"));
 
-//builder.Services.AddHttpClient<PolygonApi>((serviceProvider, client) =>
-//{
-//    var config = builder.Configuration;
-//    client.BaseAddress = new Uri(config["PolygonApi:Url"]);
-//});
 builder.Services.AddHttpClient<PolygonApi>();
 builder.Services.AddScoped<IPolygonApi, PolygonApi>();
 builder.Services.AddCors();
@@ -40,36 +35,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action}/{id?}");
-
-app.MapGet("/debug/routes", (IEnumerable<EndpointDataSource> endpointSources) =>
-{
-    var sb = new StringBuilder();
-    var endpoints = endpointSources.SelectMany(es => es.Endpoints);
-    foreach (var endpoint in endpoints)
-    {
-        if (endpoint is RouteEndpoint routeEndpoint)
-        {
-            _ = routeEndpoint.RoutePattern.RawText;
-            _ = routeEndpoint.RoutePattern.PathSegments;
-            _ = routeEndpoint.RoutePattern.Parameters;
-            _ = routeEndpoint.RoutePattern.InboundPrecedence;
-            _ = routeEndpoint.RoutePattern.OutboundPrecedence;
-        }
-    
-        var routeNameMetadata = endpoint.Metadata.OfType<Microsoft.AspNetCore.Routing.RouteNameMetadata>().FirstOrDefault();
-        _ = routeNameMetadata?.RouteName;
-    
-        var httpMethodsMetadata = endpoint.Metadata.OfType<HttpMethodMetadata>().FirstOrDefault();
-        _ = httpMethodsMetadata?.HttpMethods; // [GET, POST, ...]
-    
-        // There are many more metadata types available...
-    }
-});
-
-//app.MapFallbackToFile("index.html"); ;
 
 app.Run();
